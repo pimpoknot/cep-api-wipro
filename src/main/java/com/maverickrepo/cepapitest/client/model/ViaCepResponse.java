11dfbc78826d32616f016ea13b612eb172183d79
Marcos Maverick <marquinhopimpo@gmail.com>
2023-03-14 08:38:01 -0300

    create ViaCepResponse Entity

---
+++ src/main/java/com/maverickrepo/cepapitest/client/model/ViaCepResponse.java
@@ -0,0 +1,19 @@
+package com.maverickrepo.cepapitest.client.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ViaCepResponse {
+    private String cep;
+    private String logradouro;
+    private String complemento;
+    private String bairro;
+    private String localidade;
+    private String uf;
+    private boolean erro;
+}
