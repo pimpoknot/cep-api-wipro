5776be4a841563edebabfe97d14e06703c34ee6d
Marcos Maverick <marquinhopimpo@gmail.com>
2023-03-14 08:41:24 -0300

    Swagger Config and App Config

---
+++ src/main/java/com/maverickrepo/cepapitest/CepapitestApplication.java
@@ -0,0 +1,13 @@
+package com.maverickrepo.cepapitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CepapitestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CepapitestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/maverickrepo/cepapitest/CepapitestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maverickrepo.cepapitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CepapitestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
